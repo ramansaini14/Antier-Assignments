ff7fe77f0eb7e632183b4acfcd6be5ec7cb4f30c
ramansaini14 <ramandeepsinghsaini55@gmail.com>
2024-03-22 20:03:52 +0530

    myCommit

---
+++ JS/Problem8 - backgroundImageChanger/script.js
@@ -0,0 +1,20 @@
+function startChanging()
+{
+    imagess = ["image.png" , "image2.png"];
+    i=0;
+    storage=setInterval(changerImg, 2000);
+}
+
+function changerImg()
+{
+    document.body.style.background="url("+imagess[i]+")";
+    i++;
+    if(i==2)
+    {
+        i=0;
+    }
+}
+function stopChanging()
+{
+    clearInterval(storage);
+}
