65e58bed37e406d854f987d82d876b92bd46bb48
ramansaini14 <ramandeepsinghsaini55@gmail.com>
2024-03-27 01:31:51 +0530

    theCommit

--- Assignments/HomeWork - Result and Position Calculator/script.js
+++ Assignments/HomeWork - Result and Position Calculator/script.js
@@ -15,9 +15,7 @@ function addRecord()
         p.push(parseFloat((document.getElementById("phy").value)));
         c.push(parseFloat((document.getElementById("chem").value)));
         m.push(parseFloat((document.getElementById("math").value)));
-        console.log(p);
-        console.log(c);
-        console.log(m);
+        document.getElementById("addedShow").innerHTML = "YOU ADDED : " + "Physics : " + document.getElementById("phy").value + " Chemistry : " + document.getElementById("chem").value + " Maths : "+ document.getElementById("math").value;
 
     if(count==6)
     {
@@ -38,7 +36,7 @@ function calcPercentage()
         
     }
     console.log(percentages);
-        document.getElementById("showPer"+i).innerHTML="<br><br>Percentage of Student " + i + " is : " + percentages[0] + " , " + percentages[1] + " , " + percentages[2] + " , " + + percentages[3] + " , " + percentages[4];
+        document.getElementById("showPer"+i).innerHTML="Percentage of Student " + i + " is : " + percentages[0] + " , " + percentages[1] + " , " + percentages[2] + " , " + + percentages[3] + " , " + percentages[4];
     
 }
 
@@ -46,5 +44,21 @@ function findToppers()
 {
         percentages=percentages.sort(function (a,b) {return a-b;});
         
-        
+        if(percentages[4] === percentages[3])
+        {
+            document.getElementById("toppers").innerHTML="The Toppers are : " + " <br> " + "First and Second both are Topper with Percentage " + percentages[4] + " <br> " + " Second Topper with Percentage " + percentages[2] + " <br> " +" Third Topper with Percentage " + percentages[1] + " <br> ";
+        }
+        else if(percentages[3] === percentages[2])
+        {
+            document.getElementById("toppers").innerHTML="The Toppers are : " + " <br> " + "First Topper with Percentage " + percentages[4] + " <br> " + " Second and Third both are Topper with Percentage " + percentages[3] + " <br> " +" Third Topper with Percentage " + percentages[1] + " <br> ";
+        }
+        else if(percentages[2] === percentages[1])
+        {
+            document.getElementById("toppers").innerHTML="The Toppers are : " + " <br> " + "First Topper with Percentage " + percentages[4] + " <br> " + " Second Topper with Percentage " + percentages[3] + " <br> " +" Two Third Toppers with Percentage " + percentages[2] + " <br> ";
+        }
+        else
+        {
+            document.getElementById("toppers").innerHTML="The Toppers are : " + " <br> " + "First Topper with Percentage " + percentages[4] + " <br> " + " Second Topper with Percentage " + percentages[3] + " <br> " +" Third Topper with Percentage " + percentages[2] + " <br> ";
+        }
+
 }
