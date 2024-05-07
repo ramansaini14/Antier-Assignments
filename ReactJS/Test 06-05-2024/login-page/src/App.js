b7f5903c012db7d1a69fa815721fe5d67a4cb1d9
ramansaini14 <ramandeepsinghsaini55@gmail.com>
2024-05-08 00:29:34 +0530

    testCommit

--- ReactJS/Test 06-05-2024/login-page/src/App.js
+++ ReactJS/Test 06-05-2024/login-page/src/App.js
@@ -1,24 +1,41 @@
-import React, { useState } from "react";
 import './App.css';
+import React from "react";
 import isValid from "./emailValidator";
+import FormLayout from "./loginForm";
 
 function App() {
-  const [email, setEmail] = useState(""); 
+  const [email, setEmail] = React.useState('');
+  const [isAuth, setIsAuth] = React.useState(false);
 
-  const handleSubmit = (e) => {
+  const handleBtnClick = (e) => {
     e.preventDefault();
-    isValid(email);
+    const isValidEmail = isValid(email);
+    if (isValidEmail) {
+      setIsAuth(true);
+    } else {
+      alert("You are not allowed to access this page");
+    }
+  };
+
+  const handleInputChange = (e) => {
+    setEmail(e.target.value);
   };
 
   return (
     <div className="App">
-      <h2>Enter your email</h2>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="Email">Email : </label>
-        <input type="text" id="Email" value={email} onChange={(e) => setEmail(e.target.value)}
-        />
-        <button type="submit">OK!</button>
-      </form>
+      {!isAuth && (
+        <div>
+          <h2>Enter your email</h2>
+          <form>
+            <label htmlFor="Email">Email : </label>
+            <input type="text" id="Email" onChange={handleInputChange}></input>&nbsp;
+            <button onClick={handleBtnClick} class="btn">OK!</button>
+          </form>
+        </div>
+      )}
+      {isAuth && (
+        <FormLayout />
+      )}
     </div>
   );
 }
--- ReactJS/Test 06-05-2024/login-page/src/emailValidator.js
+++ ReactJS/Test 06-05-2024/login-page/src/emailValidator.js
@@ -1,13 +1,10 @@
-import React from "react";
-import FormLayout from "./loginForm";
-
 function isValid(email) {
     console.log(email);
+    // debugger
     if (email === "admin@123") {
-        return <FormLayout/>;
-    } else {
-        return alert("You are not allowed to access this page")
-    }
+        return true;
+    } 
+    return false;
 }
 
 export default isValid;
